816bcf696a373106452aacd890550ddbb42d1a42
Leo <takib77@gmail.com>
2021-02-15 21:51:47 +0100

    Class, Components, Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/model/hero.ts
@@ -0,0 +1,15 @@
+export class Hero {
+    id: number = 0;
+    name: string = '';
+    superPower: string = '';
+    address: string = '';
+
+    constructor(properties?: Hero) {
+        if (properties) {
+            this.id = properties.id || 0;
+            this.name = properties.name || '';
+            this.superPower = properties.superPower || '';
+            this.address = properties.address || '';
+        }
+    }
+}
